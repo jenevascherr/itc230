87f873c7347dec3ed085d192602e2dd7d9e919cc
Jeneva <jenevascherr@github.com>
2018-08-15 14:20:03 -0700

    Assignment 7 Updates

--- reactApp/index.js
+++ reactApp/index.js
@@ -13,21 +13,22 @@ app.set('port', process.env.PORT || 3000);
 app.use(express.static(__dirname + '/public'));
 app.use(require("body-parser").urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use('/api', require("cors")());
 app.use((err, req, res, next) => {
   console.log(err)
 });
 
 // set up handlebars template engine
-let handlebars = require("express-handlebars");
+const handlebars = require("express-handlebars");
 app.engine(".html", handlebars({extname: ".html"}));
 app.set("view engine", ".html");
 
 // static file for home.html
 app.get('/', (req, res, next) => {
-    Student.find((err, students) => {
-        console.log(students);
+    Student.find((err, items) => {
+        console.log(items);
         if (err) return next(err);
-        res.render('home', {students: JSON.stringify(students)});
+        res.render('home', {items: JSON.stringify(items)});
     });
 });
 
@@ -39,19 +40,19 @@ app.get('/about', (req, res) => {
 
 // GET response for get path
 app.get('/get', (req, res) => {
-    Student.findOne({ name: req.query.name }, (err, students) => {
+    Student.findOne({ name: req.query.name }, (err, items) => {
         if (err) return next(err);
         res.type('text/html');
-        res.render('details', {found: students} );
+        res.render('details', {found: items} );
     });
 });
 
 // POST response for get path
 app.post('/get', (req, res) => {
-    Student.findOne({ name: req.body.name }, (err, students) => {
+    Student.findOne({ name: req.body.name }, (err, items) => {
         if (err) return next(err);
         res.type('text/html');
-        res.render('details', {found: students} );
+        res.render('details', {found: items} );
     });
 });
 
@@ -74,48 +75,101 @@ app.get('/delete', (req, res) => {
 
 // APIs
 // get all items
-app.get('/api/students', (req, res, next) => {
+app.get('/api/items', (req, res, next) => {
     let name = req.params.name;
     console.log(name);
-    Student.find((err, results) => {
-        if (err || !results) return next(err);
-        res.json(results);
+    Student.find((err, items) => {
+        if (err) return next(err);
+        res.json(items);
     })
 });
 
 // get a single item
-app.get('/api/student/:name', (req, res, next) => {
+app.get('/api/items/:name', (req, res, next) => {
     let name = req.params.name;
-    Student.findOne({name: name}, (err, found) => {
-        if (err || !found) return next(err);
-        res.json(found);
+    Student.findOne({name: req.params.name}, (err, item) => {
+        if (err) return next(err);
+        if (item) {
+        res.json({name:item.name, family:item.family, grade:item.grade, course:item.course, year:item.year});
+        }else{
+        	res.json({"err": "Student not enrolled", "query":req.params.name});
+        }
     });
 });
 
 // delete an item
 app.get('/api/delete/:name', (req, res, next) => {
-    Student.remove({"name":req.params.name}, (err, found) => {
+    Student.remove({name: req.params.name}, (err, item) => {
         if (err) return next(err);
-        res.json({"deleted": found.result});
+        // return number of items deleted, or error if 0
+        if (item.n != 0) {
+        res.json({deleted: item.n});
+        } else {
+            res.json({"err": "No such student to delete"});
+        }
     });
 });
+
+
+
+
+app.get('/api/delete/:id', (req, res, next) => {
+    Student.remove({"_id":req.params.id}, (err, result) => {
+        if (err) return next(err);
+        res.json({"deleted": result.result.n});
+    });
+});
+
+// add an item
+app.post('/api/add/', (req, res, next) => {
+    if (!req.body._id) {
+        let item = new Student({
+                                _id: req.body._id,
+                                name:req.body.name,
+                                family:req.body.family,
+                                grade:req.body.grade,
+                                course:req.body.course,
+                                year:req.body.year
+                                });
+        item.save((err, newStudent) => {
+            if (err) return next(err);
+            console.log(newStudent);
+            res.json({updated: 0, _id: newStudent._id});
+        });
+    } else { 
+        Student.updateOne({ _id: req.body._id}, {
+                                                name:req.body.name,
+                               				    family:req.body.family,
+                                				grade:req.body.grade,
+                                				course:req.body.course,
+                                				year:req.body.year 
+                                                }, 
+            (err, result) => {
+            if (err) return next(err);
+            res.json({updated: result.nModified, _id: req.body._id});
+        });
+    }
+});
+
 
 // add or update an item
 app.get('/api/add/:name/:family/:grade/:course/:year', (req, res, next) => {
     let name = req.params.name;
-    Student.update({name: name}, {name:name, family: req.params.family, grade: req.params.grade, course: req.params.course, year: req.params.year}, {upsert: true}, (err, result) => {
+    Student.update({name: name}, 
+                   {name:name, family: req.params.family, grade: req.params.grade, course: req.params.course, year: req.params.year}, {upsert: true}, (err, result) => {
         if (err) return next(err);
         res.json({updated: result.n});
     });
 });
 
-
+/*
 // USE response for 404 handler
 app.use((req, res) => {
     res.type('text/plain');
     res.status(404);
     res.send('404 - Page Not Found');
 });
+*/
 
 // start server
 app.listen(app.get('port'), () => {
